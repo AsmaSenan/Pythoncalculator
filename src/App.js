c1562c876cbc245b357475e98a751b3d30a57444
AsmaSenan <Asma.senan18@gmail.com>
2024-09-01 00:05:03 +0300

    Fix mistyping + importing components and containers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Header, Blog, Features, Possibility, WhatGPT3, Footer } from './containers';
+import { Navbar, Feature, CTA, Article, Brand} from './components';
+
 
 const App = () => {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div className="App">
+      <div className="gradient__bg">
+          <Navbar />
+          <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+      
     </div>
   )
 }
