b33ecab128ef2c14aa7508c815cee77505354a24
AsmaSenan <Asma.senan18@gmail.com>
2024-09-03 23:55:35 +0300

    coding what is gpt? section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './whatGPT3.css';
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id="gpt3">
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature  />
+        </div>
+        <div className="gpt3__whatgpt3-heading">
+          <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+          <p>Explore The Library</p>
+        </div>
+          {/* <h1>What is GPT?</h1>
+        <p>
+        We so opinion friends me message as delight. 
+        Whole front do of plate heard oh ought.
+         His defective nor convinced residence own. 
+         Connection has put impossible own apartments boisterous.
+          At jointure ladyship an insisted so humanity he.
+         Friendly bachelor entrance to on by.
+        </p> */}
+
+      <div>
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
   )
 }
 
